fdf96ee29a9a55205b6430ba651c29062a520f9a
lilliw2-admin <lilliw2-admin@ET201-14.inst.hssoe.uci.edu>
2019-02-16 00:27:02 -0800

    populated listview with examples, but no custom adapter

--- app/src/main/java/com/example/todoquadrant/MainActivity.java
+++ app/src/main/java/com/example/todoquadrant/MainActivity.java
@@ -2,12 +2,30 @@ package com.example.todoquadrant;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<Task> tasks;
+    private ArrayAdapter<Task> tasksAdapter;
+    private ListView lvTasks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tasks = new ArrayList<Task>();
+        lvTasks = findViewById(R.id.lvTasks);
+        tasksAdapter = new ArrayAdapter<Task>(this, android.R.layout.simple_list_item_1, tasks);
+        lvTasks.setAdapter(tasksAdapter);
+
+        tasks.add(new Task("Sketch out GUI"));
+        tasks.add(new Task("Write code"));
+        tasks.add(new Task("Call Mom after dinner"));
+
     }
 }
--- app/src/main/java/com/example/todoquadrant/Task.java
+++ app/src/main/java/com/example/todoquadrant/Task.java
@@ -1,4 +1,14 @@
 package com.example.todoquadrant;
 
 public class Task {
+
+    String message;
+
+    public Task(String message){
+        this.message = message;
+    }
+
+    public String getMessage(){
+        return message;
+    }
 }
