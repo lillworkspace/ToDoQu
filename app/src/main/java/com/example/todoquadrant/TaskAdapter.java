6879f81393853b24f755ffe738b68ac23f409150
lilliw2-admin <lilliw2-admin@ET201-14.inst.hssoe.uci.edu>
2019-02-17 09:18:14 -0800

    color coded tasks, changed label

--- app/src/main/java/com/example/todoquadrant/TaskAdapter.java
+++ app/src/main/java/com/example/todoquadrant/TaskAdapter.java
@@ -1,6 +1,8 @@
 package com.example.todoquadrant;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.support.annotation.ColorInt;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -34,6 +36,24 @@ public class TaskAdapter extends ArrayAdapter<Task> {
         TextView description = taskItem.findViewById(R.id.textView_id);
         description.setText(currentTask.getDescription());
 
+        int priority = currentTask.getPriority();
+        switch (priority){
+            case 1:
+                taskItem.setBackgroundColor(Color.parseColor("#FF9AA2"));
+                break;
+            case 2:
+                taskItem.setBackgroundColor(Color.parseColor("#FFDAC1"));
+                break;
+            case 3:
+                taskItem.setBackgroundColor(Color.parseColor("#E2F0CB"));
+                break;
+            case 4:
+                taskItem.setBackgroundColor(Color.parseColor("#B5EAD7"));
+                break;
+            default:
+                taskItem.setBackgroundColor(Color.LTGRAY);
+                break;
+        }
         return taskItem;
     }
 
